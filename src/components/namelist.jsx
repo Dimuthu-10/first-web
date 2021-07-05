bb1649c8613115af4f488727678a57c7e18b97dc
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-05 16:00:30 +0530

    created namelist.jsx

---
+++ src/components/namelist.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const namelist = () =>{
+    return(
+        <React.Fragment>
+            <ul>
+                <img src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="name" />
+                <li>Name - Dimuthu</li>
+                <li>Age - 20</li>
+                <li>Gender - male</li>
+                <li>Location - Ratnapura </li>
+                <li>Email - nothing@gmail.com</li>
+            </ul>
+        </React.Fragment>
+    );
+}
+
+export default namelist;
