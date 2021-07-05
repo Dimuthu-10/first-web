e1f5112281a2a5b169d72ce403a7441f45a24a23
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-05 21:59:36 +0530

    added object array and created separate funtion

--- src/components/namelist.jsx
+++ src/components/namelist.jsx
@@ -4,42 +4,51 @@ import Items from './namelistItems'
 
 
 const namelist = () =>{
-    const NameList = {
-        gender: "male",
-        name: {
-            title: "mr",
-            first: "brad",
-            last: "gibson"
-          },
-          location: {
-            street: "9278 new road",
-            city: "kilcoole",
-            state: "waterford",
-            postcode: "93027",
-          },
-          email: "brad.gibson@example.com",
-          dob: {
-            date: "1993-07-20T09:44:18.674Z",
-            age: 26
-          },
-          picture: {
-            medium: "https://randomuser.me/api/portraits/men/75.jpg",
-          },
-    };
     return(
-        <React.Fragment>
-            <ul>
-               <Items
-                    Name = {NameList.name.title +". "+NameList.name.first+" "+NameList.name.last}
-                    Gender = {NameList.gender}
-                    City = {NameList.location.city}
-                    Email = {NameList.email}
-                    avatar = {NameList.picture.medium}
-               /> 
-              
-            </ul>
-        </React.Fragment>
+      <React.Fragment>
+        {item()}
+      </React.Fragment>
+       
     );
 }
+
+const item =()=>{
+  const NameList = [
+    {
+      gender: "male",
+      name: {title: "mr", first: "brad",last: "gibson"},
+      location: {street: "9278 new road",city: "kilcoole",state: "waterford",postcode: "93027"},
+      email: "brad.gibson@example.com",
+      dob: {date: "1993-07-20T09:44:18.674Z",age: 26},
+      picture: {medium: "https://randomuser.me/api/portraits/men/75.jpg",},
+    },
+    {
+      gender: "female",
+      name: {title: "Ms", first: "Imelda",last: "Schwandt"},
+      location: {street: "1783 new road",city: "Schwalmstadt",state: "waterford",postcode: "93027"},
+      email: "imelda.schwandt@example.com",
+      dob: {date: "1985-06-16T18:15:37.870Z",age: 36},
+      picture: {medium: "https://randomuser.me/api/portraits/women/88.jpg",},
+    },
+    {
+      gender: "female",
+      name: {title: "Ms", first: "Michele",last: "Jennings"},
+      location: {street: "8941 new road",city: "Geelong",state: "waterford",postcode: "93027"},
+      email: "michele.jennings@example.com",
+      dob: {date: "1970-08-01T09:22:30.326Z",age: 51},
+      picture: {medium: "https://randomuser.me/api/portraits/women/77.jpg",},
+    },
+];
+  return NameList.map((listItem)=>{
+    return(<Items
+      Name={listItem.name.title + ". " + listItem.name.first + " " + listItem.name.last}
+      Gender={listItem.gender}
+      City={listItem.location.city}
+      Email={listItem.email}
+      avatar={listItem.picture.medium} 
+    />)
+      
+    })
+  };
 
 export default namelist;
