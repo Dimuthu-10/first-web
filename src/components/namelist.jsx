39a3c82f91285c738888e65f092701aaf73d54d7
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-05 16:56:25 +0530

    added list items

--- src/components/namelist.jsx
+++ src/components/namelist.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import Items from './namelistItems'
+
+
 
 const namelist = () =>{
+    const NameList = {
+        gender: "male",
+        name: {
+            title: "mr",
+            first: "brad",
+            last: "gibson"
+          },
+          location: {
+            street: "9278 new road",
+            city: "kilcoole",
+            state: "waterford",
+            postcode: "93027",
+          },
+          email: "brad.gibson@example.com",
+          dob: {
+            date: "1993-07-20T09:44:18.674Z",
+            age: 26
+          },
+          picture: {
+            medium: "https://www.marketing91.com/wp-content/uploads/2016/01/download-1.jpg",
+          },
+    };
     return(
         <React.Fragment>
             <ul>
-                <img src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="name" />
-                <li>Name - Dimuthu</li>
-                <li>Age - 20</li>
-                <li>Gender - male</li>
-                <li>Location - Ratnapura </li>
-                <li>Email - nothing@gmail.com</li>
+               <Items
+                    Name = {NameList.name.title +". "+NameList.name.first+" "+NameList.name.last}
+                    Gender = {NameList.gender}
+                    City = {NameList.location.city}
+                    Email = {NameList.email}
+                    avatar = {NameList.picture.medium}
+               /> 
+              
             </ul>
         </React.Fragment>
     );
