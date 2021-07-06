dba3fac2418d2fd66cd770fe478c92e265cf6da8
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-06 15:03:59 +0530

    added styling

--- src/components/namelistItems.jsx
+++ src/components/namelistItems.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const nameListItems =(props)=>{
     return(
         <React.Fragment>
-            <li className=" list-group-item">
+            <li className=" list-group-item shadow-sm">
                 <div className="row align-items-center">
                     <div className="col-3">
-                        <img src= {props.avatar}  alt="avatar" />
+                        <img className="border border-primary border-1 rounded-circle shadow-md" src= {props.avatar}  alt="avatar" />
                     </div>
-                    <div className="col-9">
+                    <div className="col-9 font-monospace">
                         <h4>Name : {props.Name}</h4>
                         <p>Email :  {props.Email} <br /> 
                             Gender : {props.Gender} <br />
