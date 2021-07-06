329b6362349bcd997134f3658e4a086f69086045
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-06 15:15:11 +0530

    add responsiveness for all devices

--- src/components/namelistItems.jsx
+++ src/components/namelistItems.jsx
@@ -5,10 +5,10 @@ const nameListItems =(props)=>{
         <React.Fragment>
             <li className=" list-group-item shadow-sm">
                 <div className="row align-items-center">
-                    <div className="col-3">
+                    <div className="col-sm-4 col-md-6 col-lg-3">
                         <img className="border border-primary border-1 rounded-circle shadow-md" src= {props.avatar}  alt="avatar" />
                     </div>
-                    <div className="col-9 font-monospace">
+                    <div className="col-sm-4 col-md-6 col-lg-9 font-monospace">
                         <h4>Name : {props.Name}</h4>
                         <p>Email :  {props.Email} <br /> 
                             Gender : {props.Gender} <br />
