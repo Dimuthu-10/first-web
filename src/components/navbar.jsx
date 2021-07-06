b49e3e762ddac979148d4bcd4f553114eed5102d
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-07-06 15:03:25 +0530

    add responsivenes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,8 +2,20 @@ import React from'react';
 
 function navbar(){
     return (
-        <nav className="navbar navbar-light bg-light">
-            <span className="navbar-brand mb-0 h1">Name List</span>
+        <nav className="navbar p-3 mb-2 navbar-expand-lg navbar-dark bg-primary">
+            <span className="navbar-brand mb-0 fs-3">Name List</span>
+            <a className="navbar-brand" href="https://getbootstrap.com/docs/5.0/components/navbar/">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-link active" aria-current="page" href="https://getbootstrap.com/docs/5.0/components/navbar/">Home</a>
+                    <a className="nav-link" href="https://getbootstrap.com/docs/5.0/components/navbar/">Features</a>
+                    <a className="nav-link" href="https://getbootstrap.com/docs/5.0/components/navbar/">Pricing</a>
+                    <a className="nav-link disabled" href="https://getbootstrap.com/docs/5.0/components/navbar/" tabindex="-1" aria-disabled="true">Disabled</a>
+                </div>
+            </div>
         </nav>
         
     )
